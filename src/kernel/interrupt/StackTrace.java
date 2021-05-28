87a70de20100faddad1454c56edba4fec1ceca69
AlexanderWalk <alexander-walk@aon.at>
2021-05-28 15:54:18 +0200

    Fixed Stacktrace

--- src/kernel/interrupt/StackTrace.java
+++ src/kernel/interrupt/StackTrace.java
@@ -4,13 +4,12 @@ import output.console.DebugConsole;
 
 public class StackTrace {
     private static final int stackStart=0x9BFFC;
+    private static final int interreuptEIPOffset = 9*4;
+    private static final int EIPOffset = 4;
 
     static void printStacktrace(int EBP){
-        int oldEBP;
-        //ebp points to old ebp
         //old eip in interrupt Stackframe is 9 addresses away -> 9*4=36 Bytes
-        int EIP = MAGIC.rMem32(EBP+36);
-        EBP = MAGIC.rMem32(EBP);
+        int EIP = MAGIC.rMem32(EBP+interreuptEIPOffset);
         //iterate through EBP until first stackframe reached
         do {
             DebugConsole.debugPrint("EBP: ");
@@ -18,10 +17,9 @@ public class StackTrace {
             DebugConsole.debugPrint(" | EIP: ");
             DebugConsole.debugPrintlnHex(EIP);
             //EBP points to base of previous Stackframe
-            oldEBP = EBP;
             EBP= MAGIC.rMem32(EBP);
             //old eip in normal Method is 1 address away -> 1*4 Bytes
-            EIP= MAGIC.rMem32(EBP+4);
+            EIP= MAGIC.rMem32(EBP+EIPOffset);
             //TODO oldEBP < EBP
         } while(EBP<=stackStart && EBP>0 );
     }
