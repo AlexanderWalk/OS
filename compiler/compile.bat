d44e8b0eae341ed5223d91bfcefbeb9522cd14a9
AlexanderWalk <alexander-walk@aon.at>
2021-06-07 14:21:27 +0200

    Added compile batch to repo

---
+++ compiler/compile.bat
@@ -0,0 +1,4 @@
+cd "C:\FH Kempten\Master of Disaster\Betreibssystem Eigenbau\compiler"
+compile "..\src" "..\javaCompatibility" -o boot -u rte -t ia32 -T nsop -s 1M
+cd "C:\Program Files\qemu"
+qemu-system-x86_64.exe -m 32 -boot a -serial stdio -fda "C:\FH Kempten\Master of Disaster\Betreibssystem Eigenbau\compiler\BOOT_FLP.IMG"
