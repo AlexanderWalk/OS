6308473d860b99ac0f374c244a5e5a069297cf4e
AlexanderWalk <alexander-walk@aon.at>
2021-06-08 11:30:36 +0200

    Added VesaDriver #2

---
+++ src/output/vesa/GraphicsDriver.java
@@ -0,0 +1,13 @@
+package output.vesa;
+
+public abstract class GraphicsDriver {
+  public abstract void setTextMode();
+  public abstract void setGraphics320x200Mode();
+  public abstract void setPixel(int x, int y, int col);
+  public abstract void drawLine(int y, int[] col);
+  public abstract int getStartAddress();
+  
+  public boolean setMode(int xRes, int yRes, int colDepth, boolean graphical) {
+    return false; //default: mode not supported
+  }
+}
