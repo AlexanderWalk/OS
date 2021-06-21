8a2cb2e373a80a6dac8cffe48dd29dae879b77ec
AlexanderWalk <alexander-walk@aon.at>
2021-06-21 22:17:40 +0200

    added Bitmaps and binary input

--- src/binimp/ByteData.java
+++ src/binimp/ByteData.java
@@ -3,4 +3,9 @@ package binimp;
 @SJC.IgnoreUnit
 public class ByteData {
     public static byte[] gandalf;
+    public static byte[] iconBorder;
+    public static byte[] iconBorderActive;
+    public static byte[] mouseIcon;
+    public static byte[] pencilIcon;
+    public static byte[] spaceButton;
 }
