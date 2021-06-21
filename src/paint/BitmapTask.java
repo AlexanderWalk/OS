f614ebbcbc12cb1eed59f6c6531339b11e4d9374
AlexanderWalk <alexander-walk@aon.at>
2021-06-21 22:20:10 +0200

    added Task to draw Bitmaps

---
+++ src/paint/BitmapTask.java
@@ -0,0 +1,35 @@
+package paint;
+
+import output.vesa.VESAModeSelector;
+import paint.bitmap.Bitmap;
+import scheduler.task.Task;
+
+public class BitmapTask extends Task {
+
+    private final Bitmap bitmap;
+    private final int xOffset;
+    private final int yOffset;
+
+    public BitmapTask(Bitmap bitmap, int xOffset, int yOffset){
+        this.bitmap = bitmap;
+        this.xOffset=xOffset;
+        this.yOffset=yOffset;
+    }
+
+    @Override
+    public void execute() {
+        if(PaintTask.graphics.curMode==null){
+            VESAModeSelector.standardModeSelector(bitmap.getInfoHeader());
+        }
+        this.drawBitmap();
+        this.terminateTask();
+    }
+
+    private void drawBitmap(){
+        for(int j=0;j<bitmap.getInfoHeader().getBitmapHeight();j++){
+            for(int i=0;i<bitmap.getInfoHeader().getBitmapWidth();i++){
+                PaintTask.graphics.setPixel(i+xOffset,j+yOffset,bitmap.getBitmapData().getData()[i][j]);
+            }
+        }
+    }
+}
